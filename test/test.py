cf116d9274cab04f790708ee0109e674d25e2415
DeyoraPerera <deyora.perera@gmail.com>
2026-02-09 22:03:27 -0500

    fix pwm tests clk unit

--- test/test.py
+++ test/test.py
@@ -11,11 +11,11 @@ from cocotb.utils import get_sim_time
 
 async def await_half_sclk(dut):
     """Wait for the SCLK signal to go high or low."""
-    start_time = cocotb.utils.get_sim_time(units="ns")
+    start_time = cocotb.utils.get_sim_time(unit="ns")
     while True:
         await ClockCycles(dut.clk, 1)
         # Wait for half of the SCLK period (10 us)
-        if (start_time + 100*100*0.5) < cocotb.utils.get_sim_time(units="ns"):
+        if (start_time + 100*100*0.5) < cocotb.utils.get_sim_time(unit="ns"):
             break
     return
 
@@ -89,7 +89,7 @@ async def test_spi(dut):
     dut._log.info("Start SPI test")
 
     # Set the clock period to 100 ns (10 MHz)
-    clock = Clock(dut.clk, 100, units="ns")
+    clock = Clock(dut.clk, 100, unit="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -191,7 +191,7 @@ async def test_pwm_freq(dut):
             if dut.uo_out.value[0] == 1:
                 t2 = get_sim_time(unit="ns")
                 break
-            
+
         if t2 is None: raise AssertionError("Timeout: Second rising edge not detected")
 
         period = t2 - t1
